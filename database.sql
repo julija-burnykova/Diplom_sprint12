66cddb7045ce3d4b797eee5899bdc2697992dcba
Julija <gavrilovaja@yandex.ru>
2025-02-21 21:01:23 +0100

    code

--- database.sql
+++ database.sql
@@ -1,7 +1,9 @@
 -- Задание №1
+-- Проверка отображения созданного заказа в базе данных.
 
 select c."login", count(o."inDelivery") FROM "Couriers" as c INNER JOIN "Orders" AS o ON c.id = o."courierId" WHERE o."inDelivery" = TRUE GROUP BY o."inDelivery", c."login";
 
 -- Задание №2
+-- Порверка корректной записи статусов заказов в базе данных
 
 select "Orders".track,  CASE WHEN "Orders".finished = TRUE THEN 2 WHEN "Orders".cancelled = TRUE THEN -1 WHEN "Orders"."inDelivery" = True THEN 1 ELSE 0 END FROM "Orders";
